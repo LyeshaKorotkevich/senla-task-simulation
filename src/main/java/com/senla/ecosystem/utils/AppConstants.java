dae2717bed6b4be8de07de983a5b2e6a513b657b
Karatkevich Aliaksei <torlesh18112003@gmail.com>
2024-10-30 21:49:24 +0300

    feat: crodssplatform path

--- src/main/java/com/senla/ecosystem/utils/AppConstants.java
+++ src/main/java/com/senla/ecosystem/utils/AppConstants.java
@@ -2,13 +2,15 @@ package com.senla.ecosystem.utils;
 
 import lombok.experimental.UtilityClass;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @UtilityClass
 public class AppConstants {
     // Логи
-    public static final String LOG_DIR = "logs/";
+    public static final String LOG_DIR = "logs" + java.io.File.separator;
     public static final String LOG_FILE_PREFIX = "simulation_log_";
     public static final String LOG_FILE_EXTENSION = ".txt";
     public static final String LOG_DATE_FORMAT = "yyyyMMdd_HHmm";
@@ -20,11 +22,11 @@ public class AppConstants {
 
     // Файлы данных
     public static String getAnimalDataPath() {
-        return "src/main/resources/simulation" + getCurrentTimestamp() + "/Animal.txt";
+        return Paths.get("src", "main", "resources", "simulation" + getCurrentTimestamp(), "Animal.txt").toString();
     }
 
     public static String getPlantDataPath() {
-        return "src/main/resources/simulation" + getCurrentTimestamp() + "/Plant.txt";
+        return Paths.get("src", "main", "resources", "simulation" + getCurrentTimestamp(), "Plant.txt").toString();
     }
 
     private static String getCurrentTimestamp() {
